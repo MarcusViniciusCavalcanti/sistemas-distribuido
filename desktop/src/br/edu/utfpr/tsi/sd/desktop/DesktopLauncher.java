4c22ecbe380bcf9c3ed340418f5ddeb869bbca66
vinicius <vinicius@pcs-MacBook-Pro-2.local>
2019-10-17 01:01:33 -0300

    create ship and arena to game

--- desktop/src/br/edu/utfpr/tsi/sd/desktop/DesktopLauncher.java
+++ desktop/src/br/edu/utfpr/tsi/sd/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package br.edu.utfpr.tsi.sd.desktop;
 
+import br.edu.utfpr.tsi.sd.AsteroidsGame;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import br.edu.utfpr.tsi.sd.Game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Game(), config);
+		new LwjglApplication(new AsteroidsGame(), config);
 	}
 }
